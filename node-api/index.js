fa43ffe16c9835b14282e86168bf78a04d92a90b
Lubomir Kavetskiy <lubomirkavetskiy@gmail.com>
2023-09-19 20:36:29 +0300

    Create Docker-compose

--- node-api/index.js
+++ node-api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
 app.listen(port, () => {
-  console.log(`Example node API listening on port ${port}!`);
+  console.log(
+    `Example node API listening on port ${port} and NODE_ENV=${process.env.NODE_ENV}!`
+  );
 });
